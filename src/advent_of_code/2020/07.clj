0d105fe7746a36962bdd90bf0c9ca128c49b772a
ier <ivan.erakhtin@gmail.com>
2021-09-27 01:05:00 +0300

    WIP

--- src/advent_of_code/2020/07.clj
+++ src/advent_of_code/2020/07.clj
@@ -31,32 +31,58 @@
     [(first parts) (filter some? children)]))
 
 
-(defn- fltr
-  [item pattern]
-  (let [vv (filter (fn [m]
-                     (= pattern (:title m))) (second item))]
-    (when (seq vv) (first item))))
+(defn- has-bag?
+  [bags item pattern]
+  (let [fltr (fn [item pattern]
+               (let [xs (filter
+                         (fn [m] (= pattern (:title m)))
+                         (second item))]
+                 (when (seq xs)
+                   (first item))))
+        res (or (= pattern (first item))
+                (->> bags
+                     (map #(fltr % pattern))
+                     (filter seq)
+                     seq)
+                #_(...))]
+    res))
 
 
 (defn- get-containers
-  [bags pattern acc]
-  (let [direct (->> bags
+  [bags pattern]
+  (loop [items bags
+         cnt 0
+         acc 0]
+    (if (seq items)
+      (if (has-bag? bags (first items) pattern)
+        (recur (rest items) (inc cnt) (inc acc))
+        (recur (rest items) (inc cnt) acc))
+      acc)))
+
+
+#_(let [fltr (fn [item pattern]
+               (let [vv (filter #(= pattern (:title %)) (second item))]
+                 (when (seq vv) (first item))))
+        direct (->> bags
                     (map #(fltr % pattern))
                     (filter seq))]
     (if (seq direct)
-      (mapv
-       #(get-containers bags % (+ acc (count direct)))
-       direct)
-      acc)))
+      (mapv #(get-containers bags % (+ acc (count direct))) direct)
+      acc))
 
 
 (defn solve
   [input-file-name pattern]
-  (let [lines (utils/file->vec-of-str input-file-name)
-        all-bags (map parse-line lines)
-        other (get-containers all-bags pattern 0)]
-    (or (first (distinct (flatten other)))
-        0)))
+  (or (-> (->> input-file-name
+               utils/->vec-of-str
+               (map parse-line))
+          (get-containers pattern)
+          #_flatten
+          #_distinct
+          #_count)
+      0))
 
 
-(solve "resources/inputs/2020/07.txt" "shiny gold")
+(comment
+  (solve "resources/inputs/2020/07-test-sample.txt" "shiny gold")
+  )
