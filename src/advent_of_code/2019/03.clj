a1ede2afba7e9cba2c131a0132f4f133f7201ba8
ier <ivan.erakhtin@gmail.com>
2021-10-10 18:54:20 +0300

    WIP: fix, refactor

--- src/advent_of_code/2019/03.clj
+++ src/advent_of_code/2019/03.clj
@@ -10,16 +10,15 @@
       (let [value (first moves)
             [_ direction steps] (re-find #"(.)(\d+)" value)
             shift (read-string steps)
-            [dx dy] (case direction
+            [x' y'] (case direction
                       "U" [x (+' y shift)]
                       "D" [x (- y shift)]
                       "R" [(+' x shift) y]
                       "L" [(- x shift) y])
-            x' (+' x dx)
-            y' (+' y dy)]
+            acc' (conj acc [x' y'])]
         (if (empty? (rest moves))
-          (conj acc [x' y'])
-          (recur (rest moves) x' y' (conj acc [x' y'])))))))
+          acc'
+          (recur (rest moves) x' y' acc'))))))
 
 
 (defn- manhattan-distance
@@ -33,24 +32,23 @@
 
 (defn- intersections [xs]
   (let [fnx (fn [[x1 y1] [x2 y2]]
-              (and (= x1 x2)
-                   (= y1 y2)))
+              (or (= x1 x2)
+                  (= y1 y2)))
         a (first xs)
         b (second xs)
-        result (atom '())]
-    (into [] (for [la (range (count a))
-                   lb (range (count b))]
-               (when (fnx (nth a la) (nth b lb))
-                 (swap! result conj (nth a la)))))
-    @result))
+        result (into [] (for [la (range (count a))
+                              lb (range (count b))]
+                          (when (fnx (nth a la) (nth b lb))
+                            [(nth a la) (nth b lb)])))]
+    (filter some? result)))
 
 
 (defn closest-intersection-distance [xs]
   (->> xs
        (map trace-line)
        intersections
-       (map manhattan-distance)
-       (apply min)))
+       #_(map manhattan-distance)
+       #_(apply min)))
 
 
 (defn solve-1 [filename]
