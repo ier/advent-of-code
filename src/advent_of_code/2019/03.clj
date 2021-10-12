05ebb93a8f55dba1dd2b40762e53919fe509d576
ier <ivan.erakhtin@gmail.com>
2021-10-12 22:38:38 +0300

    WIP

--- src/advent_of_code/2019/03.clj
+++ src/advent_of_code/2019/03.clj
@@ -6,7 +6,7 @@
 
 (defn- trace-line [s]
   (let [xs (split s #",")]
-    (loop [moves xs x 0 y 0 acc []]
+    (loop [moves xs x 0 y 0 acc [[0 0]]]
       (let [value (first moves)
             [_ direction steps] (re-find #"(.)(\d+)" value)
             shift (read-string steps)
@@ -38,7 +38,23 @@
         (and (> ax cx bx) (> cy ay dy))) [cx ay]
     (or (and (< ay cy by) (> cx ax dx))
         (and (> ay cy by) (< cx ax dx))
-        (and (> ay cy by) (> cx ax dx))) [ax cy]))
+        (and (> ay cy by) (> cx ax dx))) [ax cy]
+    (and (= ax cx) (= ay cy)
+         (= bx cx) (= by cy)) [cx cy]
+    (and (= ax dx) (= ay dy)
+         (= bx dx) (= by dy)) [dx dy]
+    
+    (or (and (= ax bx cx) (< ay cy by))
+        (and (= ax bx cx) (> ay cy by))) [cx cy]
+        #_#_#_#_#_#_
+        (and (= ax bx dx))
+        (and (= bx ax dx))
+        
+        (and (= cx dx ax))
+        (and (= dx cx ax))
+        
+        (and (= cx dx bx))
+        (and (= dx cx bx))))
 
 
 (defn- intersections [xs]
