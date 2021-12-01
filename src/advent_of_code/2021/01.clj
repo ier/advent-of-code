094a39ebce76a72d66089a44530c302766e6da20
ier <ivan.erakhtin@gmail.com>
2021-12-01 22:26:02 +0300

    Add 2021-01 part 2

--- src/advent_of_code/2021/01.clj
+++ src/advent_of_code/2021/01.clj
@@ -2,6 +2,34 @@
   (:require
    [advent-of-code.utils :refer [->vec-of-str]]))
 
+
+(defn- sum-up
+  [data width]
+  (loop [vals data acc []]
+    (let [pair (take width vals)]
+      (if (= (count pair) width)
+        (let [frst (Integer/parseInt (first pair))
+              scnd (Integer/parseInt (second pair))
+              third (Integer/parseInt (last pair))
+              sum (+ frst scnd third)]
+          (recur (rest vals) (conj acc sum)))
+        acc))))
+
+
+(defn solve-2 [filename]
+  (let [data (->vec-of-str filename)
+        sum (sum-up data 3)]
+    (loop [vals data acc 0]
+      (let [pair (take 3 vals)]
+        (if (= (count pair) 3)
+          (let [frst (Integer/parseInt (first pair))
+                scnd (Integer/parseInt (second pair))
+                third (Integer/parseInt (last pair))
+                sum (+ frst scnd third)]
+            (recur (rest vals) (if increese? (inc acc) acc)))
+          acc)))))
+
+
 (defn solve-1 [filename]
   (let [data (->vec-of-str filename)]
     (loop [vals data acc 0]
@@ -15,4 +43,5 @@
 
 
 (comment
+  (solve-2 "resources/inputs/2021/01.txt")
   (solve-1 "resources/inputs/2021/01.txt"))
--- test/advent_of_code/2021/01_test.clj
+++ test/advent_of_code/2021/01_test.clj
@@ -1,7 +1,13 @@
 (ns advent-of-code.2021.01-test
   (:require
    [clojure.test :refer [deftest testing is]]
-   [advent-of-code.2021.01 :refer [solve-1]]))
+   [advent-of-code.2021.01 :refer [solve-1 solve-2]]))
+
+
+(deftest solve-1-test
+  (testing "solve 01 part 1"
+    (is (= 1516
+           (solve-2 "resources/inputs/2021/01.txt")))))
 
 
 (deftest solve-1-test
