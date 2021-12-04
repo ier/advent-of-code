4b4e8b82981fb6f0fe58658f4bc867797ec959d8
ier <ivan.erakhtin@gmail.com>
2021-12-05 01:02:20 +0300

    WIP 2021-04 part 1

--- src/advent_of_code/2021/04.clj
+++ src/advent_of_code/2021/04.clj
@@ -7,7 +7,12 @@
 (defn- parse-data
   [data]
   (let [numbers (str/split (first data) #",")
-        boards (map #(str/split (str/trim %) #"\s+") (filter (complement str/blank?) (rest data)))]
+        boards (->> data
+                    rest
+                    (filter (complement str/blank?))
+                    (map #(str/split (str/trim %) #"\s+"))
+                    (apply concat)
+                    vec)]
     {:numbers numbers
      :boards boards}))
 
@@ -17,30 +22,51 @@
   [xs val]
   (let [pos (positions #{val} xs)]
     (loop [idxs pos items xs]
-      (prn (first idxs))
       (if (nil? (first idxs))
         items
         (recur (rest idxs) (assoc items (first idxs) "x"))))))
 
 
+(defn- check-part
+  [v]
+  (let [limit 5
+        x-idxs [0 1 2 3 4]     ;; TODO: use range
+        y-idxs [0 5 10 15 20]  ;; TODO: use mapped range
+        xs (frequencies (map #(nth v (+ % limit)) x-idxs))
+        ys (frequencies (map #(nth v (+ % 1)) y-idxs))]
+    (or (= {"x" limit} xs)
+        (= {"x" limit} ys))))
 
 
-;;(reduce # pos)
-;; 0, 1, 2, 3, 4 | x0 x1 x2 x3 x4
-;; 
+(defn- seq-complete?
+  [xs]
+  (let [part-size 25
+        parts (partition part-size xs)]
+    (check-part (first parts))))
 
-;; 0, 5, 10, 15, 20 | x0 x5 x10 x15 x20
 
+(defn- process
+  [nums cells]
+  (loop [nums nums cells cells]
+    (let [cells' (replace-with cells (first nums))]
+      (if (seq-complete? cells')
+        (let [filtered (filter #(not= "x" %) cells')]
+          (reduce + (map #(Integer/parseInt %) filtered)))
+        (recur (rest nums) cells')))))
+
+
+(let [numbers ["7" "4" "9" "5" "11" "17" "23" "2" "0" "14" "21" "24" "10"
+               "16" "13" "6" "15" "25" "12" "22" "18" "20" "8" "19" "3" "26" "1"]
+      boards ["22" "13" "17" "11" "0" "8" "2" "23" "4" "24" "21" "9" "14" "16" "7" "6" "10" "3" "18" "5" "1" "12" "20" "15" "19"
+              "3" "15" "0" "2" "22" "9" "18" "13" "17" "5" "19" "8" "7" "25" "23" "20" "11" "10" "24" "4" "14" "21" "16" "12" "6"
+              "14" "21" "17" "24" "4" "10" "16" "15" "9" "19" "18" "8" "23" "26" "20" "22" "11" "13" "6" "5" "2" "0" "12" "3" "7"]]
+  (process numbers boards))
 
 (defn solve-1
   [filename]
-  (let [{numbers :numbers boards :boards} (->> filename ->vec-of-str parse-data)
-        res (map #(replace-with boards %) numbers)]
-    res))
-
-
-
-
+  (let [{nums :numbers cells :boards} (->> filename ->vec-of-str parse-data)
+        procesed (process nums cells)]
+    procesed))
 
 
 (comment
