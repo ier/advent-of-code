0904256a798ee20ddbdf7bc2a0be232aa194c489
ier <ivan.erakhtin@gmail.com>
2022-05-23 13:26:05 +0300

    Update project.clj metadata

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject advent-of-code "0.1.0-SNAPSHOT"
   :description "Advent of Code solutions"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :url "https://github.com/ier/advent-of-code"
+  :license {:name "MIT"
+            :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [gnuplot "0.1.3"]
                  [riddley "0.2.0"]]
